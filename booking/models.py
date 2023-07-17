ff66cae4495f2f02e2ba3a3cd48454d51e3ad7d1
SiobhanP1 <siobhanphillips11@gmail.com>
2023-07-17 16:43:10 +0000

    Create 'booking' model in models.py

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+NUMBER_OF_GUESTS = ((1, "One"), (2, "Two"), (3, "Three"), (4, "Four"))
+STATUS = ((1, "Confirmed"), (0, "Waiting list"))
+
+
+class Booking(models.Model):
+
+    guest = models.ForeignKey( User, on_delete = models.CASCADE, related_name = "bookings")
+    booked_on = models.DateTimeField(auto_now_add = True)
+    last_edited = models.DateTimeField(auto_now = True)
+    num_of_guests = models.IntegerField(choices=NUMBER_OF_GUESTS, default=1)
+    booking_status = models.IntegerField(choices=STATUS, default=1)
+    event = models.CharField(max_length=100)
+
+    
+    class Meta:
+        ordering = ['-booked_on']
+
+    def __str__(self):
+        return self.title
