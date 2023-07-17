823b0219abc1478d0db48678e7f171c1827556fc
SiobhanP1 <siobhanphillips11@gmail.com>
2023-07-17 19:22:00 +0000

    Create superuser and BookingAdmin class in admin.py

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Booking
 
-# Register your models here.
+
+admin.site.register(Booking)
+
+
+class BookingAdmin(admin.ModelAdmin):
+
+    list_display = ('event', 'booked_on', 'guest', 'num_of_guests', 'status')
+    list_filter = ['status', 'event']
+
