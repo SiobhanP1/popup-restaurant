b4c8c34ea5dbbe8f074f62055a2a2573abef51b8
SiobhanP1 <siobhanphillips11@gmail.com>
2023-07-18 16:05:02 +0000

    Create mybookings view, template and url mapping

--- booking/urls.py
+++ booking/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.EventList.as_view(), name='home')
+    path('', views.EventList.as_view(), name='home'),
+    path('login/mybookings/', views.ViewBookings.as_view(), name='my_bookings')
 ]
--- booking/views.py
+++ booking/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404 # remove?
 from django.views import generic
-from .models import Event
+from .models import Event, Booking
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class EventList(generic.ListView):
@@ -8,6 +9,14 @@ class EventList(generic.ListView):
     current_event = 'Green Square'
     template_name = 'index.html'
     model = Event
+
+
+class ViewBookings(LoginRequiredMixin, generic.ListView):
+
+    model = Booking
+    template_name = 'viewbookings.html'
+    queryset = Booking.objects.order_by('-booked_on')
+
 
 
 
