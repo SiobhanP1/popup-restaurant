fb117ba5ad0558d40100a8105005d7e01e8efcd1
SiobhanP1 <siobhanphillips11@gmail.com>
2023-07-21 10:39:35 +0000

    Set debug to false in settings.py

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-siobhanp1-popuprestaura-s4d1akix6mj.ws-eu101.gitpod.io', 
                 'popup-restaurant-booking.herokuapp.com', 
